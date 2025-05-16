090f5d60e5254830548c1a8bc788c7f4c784f143
Youssef Kotb <youssefkotb2212@gmail.com>
2025-05-16 21:45:40 +0300

    Edited app.py file

--- deployment/app.py
+++ deployment/app.py
@@ -96,8 +96,10 @@ if st.session_state.page == "Home":
 
     #for Troubleshooting
     st.write("Current working directory:", os.getcwd())
+    # /mount/src/gastric-cancer-detection-system
 
-    img_path = os.path.join(os.path.dirname(__file__), "deployment/images/flow.png")
+    img_path = os.path.join(os.path.dirname(__file__), "/images/flow.png")
+    st.write(img_path)
     st.image(img_path, use_container_width=True)
     st.caption("Figure 1: Our three-stage assessment workflow")
 
